1b91ba32a1f23006e1d3bb8535f1a2b6b588bb89
timecontinuumkerkar <timeconitnuumkerkar@gmail.com>
2022-06-14 20:48:18 +0530

    Commit 1 June 14

---
+++ HackerRank/HackerRank_Print_Full_Name.py
@@ -0,0 +1,7 @@
+def print_full_name(first, last):
+    x = f"Hello {first} {last}! You just delved into python."
+    print(x)
+
+first_name = "Karan"
+last_name = "kerkar"
+print_full_name(first_name, last_name)
